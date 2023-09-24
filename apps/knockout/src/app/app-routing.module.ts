b450ada5c66f2f8914cd2c72428d1c813ba687d9
Alexander <mauser001@hotmail.com>
2023-09-24 10:27:29 +0200

    default routing to overview

--- apps/knockout/src/app/app-routing.module.ts
+++ apps/knockout/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: '', loadComponent: () => import('./components/overview/overview.component').then(mod => mod.OverviewComponent) },
   { path: 'new', loadComponent: () => import('./components/new-tournament/new-tournament.component').then(mod => mod.NewTournamentComponent) },
   { path: 'register', loadComponent: () => import('./components/register-user-name/register-user-name.component').then(mod => mod.RegisterUserNameComponent) },
-  { path: 'details/:id', loadComponent: () => import('./components/tournament-details/tournament-details.component').then(mod => mod.TournamentDetailsComponent) }
+  { path: 'details/:id', loadComponent: () => import('./components/tournament-details/tournament-details.component').then(mod => mod.TournamentDetailsComponent) },
+  { path: '**', loadComponent: () => import('./components/overview/overview.component').then(mod => mod.OverviewComponent) },
 ];
 
 @NgModule({
